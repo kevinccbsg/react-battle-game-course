6268e84c5e6ea16c2dac88a293ec80332feffe4f
kevinccbsg <kevin2906@hotmail.com>
2017-03-19 23:09:01 +0100

    Basic configuration react router

---
+++ app/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>Hello from Main</div>
+    );
+  }
+}
+
+export default Home;
--- app/index.js
+++ app/index.js
@@ -1,75 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-
-const USER_DATA = {
-  name: 'Kevin',
-  username: 'kevinccbsg',
-  image: 'https://avatars3.githubusercontent.com/u/12685053?v=3&s=460'
-};
-
-const ProfilePic = (props) => {
-  return (
-    <img 
-      src={props.image} 
-      alt={`image of ${props.username}`} 
-    />
-  );
-};
-
-const linkStyle = {
-  color: 'blue',
-  cursor: 'pointer'
-};
-
-class Link extends Component {
-  constructor() {
-    super();
-    this.changeURL = this.changeURL.bind(this);
-  }
-
-  changeURL() {
-    window.location.replace(this.props.href);
-  }
-
-  render () {
-    return (
-      <span style={linkStyle} onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    );
-  }
-};
-
-const ProfileLink = (props) => {
-  return (
-    <div>
-      <Link href={`https://www.github.com/${props.username}`}>
-        {props.username}
-      </Link>
-    </div>
-  );
-};
-
-const ProfileName = (props) => {
-  return <h1>{props.name}</h1>;
-};
-
-const Avatar = (props) => {
-  return (
-    <div>
-      <ProfilePic image={props.image} username={props.username} />
-      <ProfileName name={props.name} />
-      <ProfileLink username={props.username} />
-    </div>
-  );
-};
+import router from './config/router';
 
 ReactDOM.render(
-  <Avatar
-    image={USER_DATA.image}
-    name={USER_DATA.name}
-    username={USER_DATA.username}
-  />,
+  router,
   document.getElementById('app')
 );
